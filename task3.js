cffe2e098e3f6a2f051aa77f132e8371e61ca18c
Julia Ohrimenko <juliajam2497@gmail.com>
2021-08-09 16:40:37 +0300

    task 3

--- task3.js
+++ task3.js
@@ -2,21 +2,22 @@ const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
-const makeTransaction = (transaction, onSuccess, onError) => {
+const makeTransaction = transaction => {
   const delay = randomIntegerFromInterval(200, 500);
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      const canProcess = Math.random() > 0.3;
 
-  setTimeout(() => {
-    const canProcess = Math.random() > 0.3;
-
-    if (canProcess) {
-      onSuccess(transaction.id, delay);
-    } else {
-      onError(transaction.id);
-    }
-  }, delay);
+      if (canProcess) {
+        res({ id: transaction.id, time: delay });
+      } else {
+        rej(transaction.id);
+      }
+    }, delay);
+  });
 };
 
-const logSuccess = (id, time) => {
+const logSuccess = ({ id, time }) => {
   console.log(`Transaction ${id} processed in ${time}ms`);
 };
 
@@ -24,13 +25,13 @@ const logError = id => {
   console.warn(`Error processing transaction ${id}. Please try again later.`);
 };
 
-/*
- * Работает так
- */
-makeTransaction({ id: 70, amount: 150 }, logSuccess, logError);
-makeTransaction({ id: 71, amount: 230 }, logSuccess, logError);
-makeTransaction({ id: 72, amount: 75 }, logSuccess, logError);
-makeTransaction({ id: 73, amount: 100 }, logSuccess, logError);
+// /*
+//  * Работает так
+//  */
+// makeTransaction({ id: 70, amount: 150 }, logSuccess, logError);
+// makeTransaction({ id: 71, amount: 230 }, logSuccess, logError);
+// makeTransaction({ id: 72, amount: 75 }, logSuccess, logError);
+// makeTransaction({ id: 73, amount: 100 }, logSuccess, logError);
 /*
  * Должно работать так
  */
@@ -41,7 +42,3 @@ makeTransaction({ id: 71, amount: 230 }).then(logSuccess).catch(logError);
 makeTransaction({ id: 72, amount: 75 }).then(logSuccess).catch(logError);
 
 makeTransaction({ id: 73, amount: 100 }).then(logSuccess).catch(logError);
-
-/*Задание 3
-Перепиши функцию makeTransaction() так, чтобы она не использовала
-callback-функции onSuccess и onError, а принимала всего один параметр transaction и возвращала промис.*/
